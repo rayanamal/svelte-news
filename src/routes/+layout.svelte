50af8d1a9105f8e335358072657998f237e04aa6
Rayan Amal <rayan.amal@icloud.com>
2026-09-24 16:58:44 +0300

    first steps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,4 +6,9 @@
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<div class="mx-auto xl:max-w-3/4">
+    <h1 class="text-center text-2xl sm:text-3xl font-semibold font-[Space_Grotesk_Variable] mb-2 mt-3">Svelte News</h1>
+</div>
+
 {@render children()}
